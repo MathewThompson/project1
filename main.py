8502fd2ba75127997bbc1f6d48679d9e2d24cbde
MathewThompson2 <115591261+MathewThompson@users.noreply.github.com>
2022-11-03 21:59:38 +0000

    Fixed Syntax Errors

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ import random
 
 keepAsking = True
 while keepAsking:
-    num = input("Type a number for an upper bound:" )
-    if num.isdigit():
-        print("Let's play!")
-        num = int(num)
-        keepAsking = False
-    else:
-        print("Invalid input. Try again.")
+  num = input("Type a number for an upper bound:")
+  if num.isdigit():
+    print("Let's play!")
+    num = int(num)
+    keepAsking = False
+  else:
+    print("Invalid input. Try again.")
 
 secret = random.randint(1, num)
 guess = None
 counter = 1
 
 while guess != secret:
-    guess = input("Type a number between 1 and " +str(num)+ ": ")
-    if guess.isdigit():
-        guess = int(guess)
-    if False: # can't always be wrong
-        print("You got it!")
-    else:
-        print("Try again.")
-        # what else?
+  guess = input("Type a number between 1 and " + str(num) + ": ")
+  if guess.isdigit():
+    guess = int(guess)
+  if False:  # can't always be wrong
+    print("You got it!")
+  else:
+    print("Try again.")
+    # what else?
 
 if count == 1:
-    print("It took you", count, "guess!")
+  print("It took you", count, "guess!")
 else:
-    print("It took you", count, "guesses!")
+  print("It took you", count, "guesses!")
